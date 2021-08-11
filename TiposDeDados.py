f0466b10d8d72069b0934d89985c4ab5bc6775a3
Filippo Valiante Filho <39290223+filippovf@users.noreply.github.com>
2021-08-11 13:24:26 -0300

    Add files via upload
    Aula 1

---
+++ TiposDeDados.py
@@ -0,0 +1,14 @@
+print("Tipos de Dados em Pyhton:\n")
+print('27 =', type(27)) # O tipo de dados do número 27 é int(numero inteiro)
+print('2.7 =', type(2.7)) # O tipo de dados do número 2.7 é int(numero float)
+print('"27" =', type('27')) # O tipo de dados docom aspas é string
+print('2.7 + 2j =', type(2.7 + 2j)) # O tipo de dados complexo usa o j
+print('True or False =', type(True)) # O tipo de dados True or False é booleano
+print(type(27)) # O tipo de dados do número 27 é int(numero inteiro)
+print(type(2.7)) # O tipo de dados do número 2.7 é int(numero float)
+print(type('27')) # O tipo de dados docom aspas é string
+print(type(2.7 + 2j)) # O tipo de dados complexo usa o j
+print(type(True)) # O tipo de dados True or False é booleano
+print("\nHello \nWorld!") # O \n pula linha como no bash
+print('Pode usar "aspas" duplas mantendo a string inteira em aspas simples e usar \\n para pular linha')
+# uma \ adicional mata a \ tratando-a como caractere comum
---
+++ Variaveis.py
@@ -0,0 +1,9 @@
+print('Declarando e usando variáveis:\n')
+aluno = "Filippo"
+pi = 3.1415926536
+status = True
+print(aluno)
+print(pi)
+print('circunferência de raio 1 =', pi * 2 * 1)
+print(2 * pi)
+print(status)
