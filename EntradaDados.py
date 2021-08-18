a5786c05afa8e3d7cc3c97e372b404b681bafd35
Filippo Valiante Filho <39290223+filippovf@users.noreply.github.com>
2021-08-18 11:18:13 -0300

    Update EntradaDados.py

--- EntradaDados.py
+++ EntradaDados.py
@@ -6,6 +6,7 @@ print("\nA gorjeta é de R$", gorjeta)
 print("\nA gorjeta é de R$", round(gorjeta, 2))     #Arredonda para "n" casas
 print("\nA gorjeta é de R$ {:.2f}".format(gorjeta)) #Formata com 2 casas decimais
 print("\nA gorjeta é de R$ {:.3f}".format(gorjeta)) #Formata com 3 casas decimais
+print(f'A gorjeta é de R$ {gorjeta:.2f}')           #Formata com 2 casas decimais
 
 teste = 10/6
 print("\n")
